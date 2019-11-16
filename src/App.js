501391a5a2674d7803f7d3c44ddc52e53e827388
Rajesh Pillai <pillai.rajesh@gmail.com>
2019-11-17 00:18:32 +0530

    Routing fixed: Pending: Tag Search

--- src/App.js
+++ src/App.js
@@ -1,55 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Blogs from './features/blogs'
-import BlogForm from './features/blogs/blog-form'
-import Posts from './features/posts';
-import PostRead from './features/posts/post-read';
-import PostEdit from './features/posts/post-edit';
-import Categories from './features/categories';
-import AppContext from './context/app-context';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch
+} from "react-router-dom";
+import Blogs from "./features/blogs";
+import BlogForm from "./features/blogs/blog-form";
+import Posts from "./features/posts";
+import Categories from "./features/categories";
+import AppContext from "./context/app-context";
 
-import './App.css';
-import { unlink } from 'fs';
+import "./App.css";
+import { unlink } from "fs";
 
-const initCategories = [
-  "Programming",
-  "Web Development"
-]
+const initCategories = ["Programming", "Web Development"];
 const initBlogs = [
   {
-    id: 1, title: "Learn Mathematics",
-    desc: "Coming soon", category: "Mathematics", subcat: "Basic Maths"
+    id: 1,
+    title: "Learn Mathematics",
+    desc: "Coming soon",
+    category: "Mathematics",
+    subcat: "Basic Maths"
   },
   {
-    id: 2, title: "Learn Angular Step By Step",
-    desc: "Coming soon", category: "Web Development", subcat: "Angular"
+    id: 2,
+    title: "Learn Angular Step By Step",
+    desc: "Coming soon",
+    category: "Web Development",
+    subcat: "Angular"
   }
-]
+];
 
 const initPosts = [
   {
-    id: 1, blogId: 1, title: "CSharp Tutorial", category: "Programming",
+    id: 1,
+    blogId: 1,
+    title: "CSharp Tutorial",
+    category: "Programming",
     content: generateContent(3),
     tags: ["csharp", "programming", "microsoft"],
     rating: 4,
     comments: [
-      { id: 1, postId: 1, author: "rajesh", content: "Nice",commentedOn:new Date() },
-      { id: 2, postId: 1, author: "jai", content: "Hi", commentedOn: new Date() },
+      {
+        id: 1,
+        postId: 1,
+        author: "rajesh",
+        content: "Nice",
+        commentedOn: new Date()
+      },
+      {
+        id: 2,
+        postId: 1,
+        author: "jai",
+        content: "Hi",
+        commentedOn: new Date()
+      }
     ]
   },
   {
-    id: 2, blogId: 1, title: "Learn ES6", category: "Web Development",
+    id: 2,
+    blogId: 1,
+    title: "Learn ES6",
+    category: "Web Development",
     content: generateContent(3),
     tags: ["javascript", "es5", "es6"],
-    rating: 5,
+    rating: 5
   },
   {
-    id: 3, blogId: 1, title: "Learn JavaScript", category: "Web Development",
+    id: 3,
+    blogId: 1,
+    title: "Learn JavaScript",
+    category: "Web Development",
     content: generateContent(3),
     tags: ["javascript"],
-    rating: 2,
+    rating: 2
   }
-]
+];
 
 function generateContent(words) {
   let content = "";
@@ -83,7 +110,6 @@ function generateContent(words) {
   return content;
 }
 
-
 function App() {
   const [blogs, setBlogs] = useState(initBlogs);
   const [posts, setPosts] = useState(initPosts);
@@ -92,100 +118,96 @@ function App() {
 
   const appState = {
     blogId: blogId,
-    setBlogId: function (blogId) {
+    setBlogId: function(blogId) {
       this.blogId = blogId;
       setBlogId(blogId);
       console.log(`setting blogid to ${this.blogId}`);
     }
-  }
+  };
 
-  const showPosts = (blogId) => {
+  const showPosts = blogId => {
     alert(blogId);
-  }
+  };
 
   const addBlog = blog => {
     blog.id = blogs.length + 1;
     setBlogs([...blogs, blog]);
-  }
+  };
 
   const deleteBlog = id => {
-    setBlogs(blogs.filter(blog => blog.id !== id))
-  }
-
+    setBlogs(blogs.filter(blog => blog.id !== id));
+  };
 
   const addPost = post => {
     console.log("Adding post: ", post);
     post.id = posts.length + 1;
     setPosts([...posts, post]);
-  }
+  };
+
+  const deletePost = id => {
+    setPosts(posts.filter(post => post.id !== id));
+  };
 
   const onPostUpdate = (blogId, postId, content) => {
-    let updatedPosts = posts.map((p) => {
+    let updatedPosts = posts.map(p => {
       if (p.id == postId && p.blogId == blogId) {
         p.content = content;
       }
       return p;
-    })
+    });
     console.log("update: ", updatedPosts);
     setPosts(updatedPosts);
-  }
+  };
 
-
-  const deletePost = id => {
-    setPosts(posts.filter(post => post.id !== id))
-  }
-
-  useEffect(() => {
-
-  }, [])
+  useEffect(() => {}, []);
 
   function loadBlog() {
-    return <>
-      <Blogs blogs={blogs} onShowPost={showPosts} deleteBlog={deleteBlog} />
-    </>
+    return (
+      <>
+        <Blogs blogs={blogs} onShowPost={showPosts} deleteBlog={deleteBlog} />
+      </>
+    );
   }
 
   function loadBlogForm() {
-    return <>
-      <BlogForm addBlog={addBlog} />
-    </>
+    return (
+      <>
+        <BlogForm addBlog={addBlog} />
+      </>
+    );
   }
 
   function loadPosts(props) {
     console.log("lp:", props);
     let blogId = Number(props.match.params.blogId);
     let p = posts.filter(post => post.blogId === blogId);
-    return <Posts blogId={blogId} posts={p} deletePost={deletePost} addPost={addPost} />
+    return (
+      <Posts
+        blogId={blogId}
+        posts={p}
+        deletePost={deletePost}
+        addPost={addPost}
+        onPostUpdate={onPostUpdate}
+      />
+    );
   }
-
-  function readPost(props) {
-    let postId = Number(props.match.params.postId);
-    let p = posts.find(post => post.id === postId);
-    console.log("Showing post: ", p);
-    return <PostRead blogId={p.blogId} post={p} />
-  }
-
-  function editPost(props) {
-    let postId = Number(props.match.params.postId);
-    let p = posts.find(post => post.id === postId);
-    return <PostEdit updatePost={onPostUpdate} blogId={p.blogId} post={p} />
-  }
-
-  const postsByTag = ({ match }) => {
-    let { blogId, tag } = match.params;
-    console.log("Tag: ", blogId, tag);
-    let p = posts.filter(post => post.blogId == blogId && post.tags.includes(tag));
-    return <Posts blogId={blogId} posts={p} deletePost={deletePost} addPost={addPost} />
-  }
-
 
   const postsByCategories = ({ match }) => {
     let { blogId, category } = match.params;
     console.log("Categories: ", blogId, category);
-    let p = posts.filter(post => post.blogId == blogId && post.category.toLowerCase() == category);
+    let p = posts.filter(
+      post => post.blogId == blogId && post.category.toLowerCase() == category
+    );
     console.log("PostByCategory: ", p);
-    return <Posts blogId={blogId} posts={p} deletePost={deletePost} addPost={addPost} />
-  }
+    return (
+      <Posts
+        blogId={blogId}
+        posts={p}
+        deletePost={deletePost}
+        addPost={addPost}
+      />
+    );
+  };
 
   return (
     <Router>
@@ -193,18 +215,20 @@ function App() {
         <div className="app">
           <header className="app-header">
             <p>Unlearning Labs</p>
-            <Link to="/">Blogs</Link>
+            <NavLink to="/">Blogs</NavLink>
           </header>
           <div className="content-wrapper">
             {appState.blogId && <Categories categories={categories} />}
             <div className="content-area">
-              <Route exact path="/" render={loadBlog} />
-              <Route path="/blog-form" render={loadBlogForm} />
-              <Route path="/blogs/:blogId/posts" render={loadPosts} />
-              <Route path="/posts/:postId/read" render={readPost} />
-              <Route path="/posts/:postId/edit" render={editPost} />
-              <Route path="/posts/:blogId/tags/:tag" render={postsByTag} />
-              <Route path="/posts/:blogId/categories/:category" render={postsByCategories} />
+              <Switch>
+                <Route exact path="/" render={loadBlog} />
+                <Route path="/blog-form" render={loadBlogForm} />
+                <Route path="/blogs/:blogId/posts" render={loadPosts} />
+                <Route
+                  path="/posts/:blogId/categories/:category"
+                  render={postsByCategories}
+                />
+              </Switch>
             </div>
           </div>
         </div>
--- src/features/posts/index.js
+++ src/features/posts/index.js
@@ -1,13 +1,20 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from "react";
+import { Route, NavLink, BrowserRouter as Router } from "react-router-dom";
 
-import PostForm from './post-form';
-import AppContext from '../../context/app-context';
+import PostForm from "./post-form";
+import AppContext from "../../context/app-context";
+import PostRead from "./post-read";
+import PostEdit from "./post-edit"; //"./features/posts/post-edit";
 
+import "./post.css";
 
-import './post.css';
-
-export default function Posts({ posts, blogId, addPost, deletePost }) {
+export default function Posts({
+  posts,
+  blogId,
+  addPost,
+  deletePost,
+  onPostUpdate
+}) {
   const appContext = useContext(AppContext);
 
   useEffect(() => {
@@ -15,32 +22,71 @@ export default function Posts({ posts, blogId, addPost, deletePost }) {
     console.log(`setting blogid to ${blogId}`);
   }, [appContext, blogId]);
 
-  let postView = posts.map((post) => {
+  let postView = posts.map(post => {
     return (
       <div className="post" key={post.id}>
         <h2>{post.title}</h2>
         <div>{post.desc}</div>
         <div>Category: {post.category}</div>
 
-        <Link to={`/posts/${post.id}/read`}>Read more..</Link>
-        <Link to={`/posts/${post.id}/edit`}><button>EDIT</button></Link>
+        <NavLink to={`/posts/${post.id}/read`}>Read more..</NavLink>
+        <NavLink to={`/posts/${post.id}/edit`}>
+          <button>EDIT</button>
+        </NavLink>
         <button onClick={() => deletePost(post.id)}>Delete</button>
-      </div >
-    )
-  })
+      </div>
+    );
+  });
 
   function loadPostForm() {
-    return (
-      <PostForm addPost={addPost} />
-    )
+    return <PostForm addPost={addPost} />;
   }
+
+  function readPost(props) {
+    let postId = Number(props.match.params.postId);
+    let p = posts.find(post => post.id === postId);
+    console.log("Showing post: ", p);
+    return <PostRead blogId={p.blogId} post={p} />;
+  }
+
+  function editPost(props) {
+    let postId = Number(props.match.params.postId);
+    console.log(`editing post with id ${postId}`);
+    let p = posts.find(post => post.id === postId);
+    return <PostEdit updatePost={onPostUpdate} blogId={p.blogId} post={p} />;
+  }
+
+  const postsByTag = ({ match }) => {
+    let { blogId, tag } = match.params;
+    console.log("Tag: ", blogId, tag);
+    let p = posts.filter(
+      post => post.blogId == blogId && post.tags.includes(tag)
+    );
+    {
+      /* <Posts
+        blogId={blogId}
+        posts={p}
+        deletePost={deletePost}
+        addPost={addPost}
+      /> */
+    }
+    return <h2>Todo</h2>;
+  };
 
   return (
     <Router>
-      <Route path='/blogs/:blogId/posts/new' render={loadPostForm} />
-      <Link to={`/blogs/${blogId}/posts`}>ALL POSTS</Link>
-      <Link to={`/blogs/${blogId}/posts/new`}><span title="new post" className="btn btn-round">+</span></Link>
+      <Route path="/blogs/:blogId/posts/new" render={loadPostForm} />
+      <Route path="/posts/:postId/read" render={readPost} />
+      <Route path="/posts/:postId/edit" render={editPost} />
+      <Route path="/posts/:blogId/tags/:tag" render={postsByTag} />
+
+      <NavLink to={`/blogs/${blogId}/posts`}>ALL POSTS</NavLink>
+      <NavLink to={`/blogs/${blogId}/posts/new`}>
+        <span title="new post" className="btn btn-round">
+          +
+        </span>
+      </NavLink>
       {postView}
     </Router>
-  )
+  );
 }
